a149bd9e8249ec7ed88de2a4dfae011b99deb73d
Arafat Kabir Khan <arafatkabir@iut-dhaka.edu>
2025-02-13 02:07:16 +0600

    blogs loaded from fake data

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
+
+const Blog = ({ blogProp }) => {
+	const { cover_image, title, author_name, author_img, posted_date, reading_time, hashtags } = blogProp;
+
+	return (
+		<div className="mb-10">
+			<img className="w-full rounded-xl" src={cover_image} alt={`cover picture of ${title}`} />
+			<div className="flex justify-between items-center mt-8">
+				<div className="flex items-center">
+					<img className="w-15 h-15 rounded-2xl object-cover" src={author_img} alt="Author's picture" />
+					<div className="ml-6">
+						<h4>{author_name}</h4>
+						<h5>{posted_date}</h5>
+					</div>
+				</div>
+				<div className="flex justify-between">
+					<h4>{reading_time} min read</h4>
+					<button className="text-xl ml-2 pt-1">
+						<CiBookmark></CiBookmark>
+					</button>
+				</div>
+			</div>
+			<h2 className="text-4xl mt-4">{title}</h2>
+			<p className="mt-4">
+				{hashtags.map((tagItem, idx) => (
+					<a key={idx} className="mr-2">
+						{tagItem}
+					</a>
+				))}
+			</p>
+			<button className="text-purple-900 underline font-bold">
+				Mark as read
+			</button>
+		</div>
+	);
+};
+
+Blog.propTypes = {
+	blogProp: PropTypes.object.isRequired
+};
+
+export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+
 const Blogs = () => {
-	
+	const [blogs, setBlogs] = useState([]);
+	useEffect(() => {
+		fetch("fakeBlogs.json")
+			.then(res => res.json())
+			.then(data => setBlogs(data));
+	}, []);
 	return (
 		<div className="md:w-2/3">
-			from blogs
+			{blogs.map(blogItem => (
+				<Blog key={blogItem.id} blogProp={blogItem}></Blog>
+			))}
 		</div>
 	);
 };
-
-export default Blogs
+export default Blogs;
