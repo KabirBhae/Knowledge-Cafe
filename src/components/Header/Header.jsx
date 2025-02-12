8495757fcb87827d5ee69ee772bcc632c4720b64
Arafat Kabir Khan <arafatkabir@iut-dhaka.edu>
2025-02-13 01:56:53 +0600

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import profilePicture from "../../assets/images/profile.png"
+
+const Header = () => {
+	return (
+		<header className="flex justify-between items-center mx-4 p-2 border-b-2">
+			<h3 className="text-3xl font-bold">Knowledge Cafe</h3>
+			<img src={profilePicture} alt="" />
+		</header>
+	)
+}
+
+export default Header
